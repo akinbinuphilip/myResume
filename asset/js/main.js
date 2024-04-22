6056afa01b0ee1d033e0e7756896e0ecfc45bb70
akinbinuphilip <akinbinup840@gmail.com>
2024-04-21 18:08:19 -0700

    worked on the contact message

--- asset/js/main.js
+++ asset/js/main.js
@@ -290,88 +290,39 @@
   //   });
   // });
 
-  /**
-   * 
-   * 
-   * (function () {
-    "use strict";
+  // Focus Icons
+  const inputs = document.querySelectorAll("input");
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll(".needs-validation");
+  console.log(typeof inputs);
 
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach(function (form) {
-        form.addEventListener(
-            "submit",
-            function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
+  Object.keys(inputs).map((key, input) => onBlurOnFocus(inputs[key]));
 
-                form.classList.add("was-validated");
-            },
-            false
-        );
-    });
-})();
-// Focus Icons
-const inputs = document.querySelectorAll("input");
-
-console.log(typeof inputs);
-
-Object.keys(inputs).map((key, input) => onBlurOnFocus(inputs[key]));
-
-function onBlurOnFocus(input) {
+  function onBlurOnFocus(input) {
     input.onfocus = (e) => {
-        e.target.previousElementSibling.classList.toggle("text-primary");
+      e.target.previousElementSibling.classList.toggle("text-primary");
     };
     input.onblur = (e) => {
-        e.target.previousElementSibling.classList.toggle("text-primary");
+      e.target.previousElementSibling.classList.toggle("text-primary");
     };
-}
-   */
-  /** Array.prototype.splice.call(Forms).forEach(function (form) {
-      form.addEventListener("submit", function (event) {
-        if (!form.checkvalidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-          console.log("");
-        }
-      });
-    });
-   * Initiate Pure Counter
-   */
+  }
+  //  Initiate Pure Counter
+
   new PureCounter();
 })();
 
-function myfunction() {
-  console.log("hello");
-}
-//  const submitButton = document.getElementById("submit_button");
-//  const Form_holder = document.getElementById("form");
-//  const Forms = document.querySelectorAll("needs-validation");
-//  submitButton.addEventListener("click", myfunction());
-
-(function () {
-  "use strict";
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
+// function myfunction(e) {
+//   e.preventDefault()
+// }
+const email = document.getElementById("email");
+const email_holder = document.getElementById("form_holder");
+email.addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("thanks");
+});
+const submitButton = document.getElementById("submit_button");
+const Form_holder = document.getElementById("form");
+const Forms = document.querySelectorAll("needs-validation");
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  alert("thanks")
+});
